775e5d63467f32c9fbe468087b2377c7b806c327
Joy Dutta <dutta_joy@hotmail.com>
2024-02-01 16:33:00 +0000

    Updates Post Edit Action method

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -91,7 +91,7 @@ namespace ByteInsights.Controllers
                 return NotFound();
             }
             ViewData["BlogId"] = new SelectList(_context.Blogs, "Id", "Description", post.BlogId);
-            ViewData["BlogUserId"] = new SelectList(_context.Users, "Id", "Id", post.BlogUserId);
+            
             return View(post);
         }
 
@@ -100,7 +100,7 @@ namespace ByteInsights.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogId,BlogUserId,Title,Abstract,Content,Created,Updated,ReadyStatus,Slug,ImageData,ContentType")] Post post)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BlogId,Title,Abstract,Content,ReadyStatus,Image")] Post post)
         {
             if (id != post.Id)
             {
@@ -110,7 +110,7 @@ namespace ByteInsights.Controllers
             if (ModelState.IsValid)
             {
                 try
-                {
+                {   post.Updated = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
                     _context.Update(post);
                     await _context.SaveChangesAsync();
                 }
